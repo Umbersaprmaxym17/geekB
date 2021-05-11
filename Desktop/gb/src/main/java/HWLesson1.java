ff3b4f57f34937fb1858bdde1dc244a25f9f5a19
Maxym Saprykin <maxym.saprykin@hypr.com>
2021-05-10 22:25:33 -0400

    add first method

--- Desktop/gb/src/main/java/HWLesson1.java
+++ Desktop/gb/src/main/java/HWLesson1.java
@@ -1,2 +1,16 @@
 public class HWLesson1 {
+
+
+    public static void main(String[] args) {
+
+        printThreeWords();
+
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 }
