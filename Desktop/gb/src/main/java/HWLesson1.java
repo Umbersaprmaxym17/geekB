5a159b0cc0f29f5f2f729f5a3f2449a78419ebd0
Maxym Saprykin <maxym.saprykin@hypr.com>
2021-05-10 22:39:19 -0400

    add  second method checkSumSigh

--- Desktop/gb/src/main/java/HWLesson1.java
+++ Desktop/gb/src/main/java/HWLesson1.java
@@ -3,11 +3,23 @@ public class HWLesson1 {
     public static void main(String[] args) {
 
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 7;
+        int b = -1;
+        if (a + b >=0 ) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
     }
 }
