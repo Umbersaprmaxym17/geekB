9d5dfd95d36811ba6b6552a2076ea316e959f7e1
Maxym Saprykin <maxym.saprykin@hypr.com>
2021-05-12 21:47:53 -0400

    rename class

--- Desktop/gb/src/main/java/com/startfast/gb/ms/Culculator.java
+++ Desktop/gb/src/main/java/com/startfast/gb/ms/Culculator.java
@@ -2,7 +2,7 @@ package com.startfast.gb.ms;
 
 import java.util.Scanner;
 
-public class FastStart {
+public class Culculator {
 
     public static void main(String[] args) {
         System.out.println("Type the operation");
