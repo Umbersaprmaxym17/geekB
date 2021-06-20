f3e0d243640681464dccf04999514fab4df5b45a
Maxym Saprykin <maxym.saprykin@hypr.com>
2021-06-20 19:01:43 +0300

    Code stil does not work

--- Desktop/javacore2/src/main/java/com/exceptions/MainClass.java
+++ Desktop/javacore2/src/main/java/com/exceptions/MainClass.java
@@ -8,16 +8,12 @@ public class MainClass{
 
     public static void main(String[] args) throws MyArraySizeException {
 
-        String[][] users = new String[5][5];
-//        users[0]  = "one";
-//        users[1]  = "two";
-//        users[2]  = "three";
-//        users[3]  = "four";
+        String[][] users = new String[4][5];
 
         users[0][0]  = "one";
         users[0][1]  = "two";
         users[0][2]  = "three";
-        users[0][3]  = "four";
+        users[0][3]  = "4";
         users[1][0]  = "five";
         users[1][1]  = "six";
         users[1][2]  = "seven";
@@ -31,25 +27,68 @@ public class MainClass{
         users[3][1]  = "fourteen";
         users[3][2]  = "fifteen";
         users[3][3]  = "eighteen";
-//        users[3][4]  = "nothing is here";
+    //    users[3][4]  = "nothing is here";
+
+        printTwoDimensionalArray(users);
 
 
-
-            printTwoDimensionalArray(users);
 
 
     }
 
     public static   void printTwoDimensionalArray(String[][] array) throws MyArraySizeException {
-        int height   = array.length;
-        int width  = array[0].length;
-        if (height > 4 && width < 4) {
-            throw new MyArraySizeException(" Array size is more or less  than 4");
-            //System.out.println(" Array size is more or less  than 4");
-        } else {
+//        int height   = array.length;
+//        int width  = array[0].length;
+//        if (height > 4 && width < 4) {
+//            throw new MyArraySizeException("Array size is less or more than 2 dimensional 4*4 ");
+//        }else {
+//            System.out.println(Arrays.deepToString(array));
+//        }
 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    int convertedStringToDigit = Integer.parseInt(Arrays.deepToString(array));
+                    System.out.print(convertedStringToDigit);
+                    System.out.println(Arrays.deepToString(array));
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    System.out.println(" Catch 1 NumberFormatException");
+                } finally {
+                    System.out.println();
+                }
+            }
         }
-            System.out.println(Arrays.deepToString(array));
+
+
+
+
+
+
+//        int height   = array.length;
+//        int width  = array[0].length;
+//        if (height > 4 && width < 4) {
+//           // throw new MyArraySizeException(" Array size is more or less  than 4");
+//            System.out.println(" Array size is more or less  than 4");
+//        } else {
+//            System.out.println(Arrays.deepToString(array));
+//        }
+//        try {
+//            System.out.println(Arrays.deepToString(array));
+//        }catch ( ArrayIndexOutOfBoundsException exception) {
+//            System.out.println(exception.getMessage());
+//        }catch (MyArraySizeException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//           try {
+//               System.out.println(Arrays.deepToString(array));
+//               throw new MyArraySizeException();
+//           } catch (MyArraySizeException ex) {
+//               System.out.println(ex.getMessage());
+//
+//           }
+
+
 
     }
 
--- Desktop/javacore2/src/main/java/com/exceptions/MyArraySizeException.java
+++ Desktop/javacore2/src/main/java/com/exceptions/MyArraySizeException.java
@@ -1,25 +1,35 @@
 package com.exceptions;
 
-import java.util.Arrays;
-
-public class MyArraySizeException extends Exception{
 
 
-    public MyArraySizeException(String s) {
-
+public class MyArraySizeException extends Throwable{
+    public MyArraySizeException(String message) {
+        super(message);
     }
 
-    public static   void findArraySize (String[][] array) throws MyArraySizeException {
-        int height   = array.length;
-        int width  = array[0].length;
-        if (height > 4 && width < 4) {
-            throw new MyArraySizeException(" Array size is more or less  than 4");
-            //System.out.println(" Array size is more or less  than 4");
-        } else {
-
-        }
-
+    public MyArraySizeException(Throwable e) {
+        super(e);
     }
+
+    public MyArraySizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    //    public MyArraySizeException(String s) {
+//
+//    }
+
+//    public static   void findArraySize (String[][] array) throws MyArraySizeException {
+//        int height   = array.length;
+//        int width  = array[0].length;
+//        if (height > 4 && width < 4) {
+//            throw new MyArraySizeException(" Array size is more or less  than 4");
+//            //System.out.println(" Array size is more or less  than 4");
+//        } else {
+//
+//        }
+//
+//    }
 
 
 }
