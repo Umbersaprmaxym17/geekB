7cfb960391bd5d4757aaf1a46fbbced80df6b5b0
Maxym Saprykin <maxym.saprykin@hypr.com>
2021-07-04 17:23:40 -0400

    add readers

--- Desktop/javacore2/src/main/java/com/serverchat/EchoClient.java
+++ Desktop/javacore2/src/main/java/com/serverchat/EchoClient.java
@@ -1,9 +1,6 @@
 package com.serverchat;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -13,6 +10,7 @@ public class EchoClient {
     private  Socket socket;
     private  DataInputStream inputStream;
     private  Scanner scanner;
+    private static String messageText;
 
     public static void main(String[] args) {
         new EchoClient();
@@ -23,6 +21,7 @@ public class EchoClient {
         openConnection();
         while (true) {
             sendMessage();
+
         }
 
     }
@@ -33,6 +32,15 @@ public class EchoClient {
             inputStream = new DataInputStream(socket.getInputStream());
             outputStream = new DataOutputStream(socket.getOutputStream());
 
+            // to send data to the server
+            final DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            // to read data coming from the server
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // to read data from the keyboard
+            final BufferedReader keyBoard = new BufferedReader(new InputStreamReader(System.in));
+
+
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -40,6 +48,17 @@ public class EchoClient {
                         while (true) {
                             final String messageFromServer = inputStream.readUTF();
                             System.out.println(messageFromServer);
+                            String str, str1;
+                            while (!(str = keyBoard.readLine()).equals("exit")) {
+
+                                // send to the server
+                                dataOutputStream.writeBytes(str + "\n");
+
+                                // receive from the server
+                                str1 = bufferedReader.readLine();
+
+                                System.out.println(str1);
+                            }
                             }
                         } catch(IOException e){
                             e.printStackTrace();
@@ -65,14 +84,18 @@ public class EchoClient {
         }
     }
 
-    private void sendMessage() {
-       final String s = scanner.nextLine();
+    private String sendMessage() {
+        final  String s = scanner.nextLine();
         try {
             outputStream.writeUTF(s);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        messageText = s;
+        return  messageText;
+    }
 
-
+    public static String getMessage() {
+        return messageText;
     }
 }
--- Desktop/javacore2/src/main/java/com/serverchat/EchoServer.java
+++ Desktop/javacore2/src/main/java/com/serverchat/EchoServer.java
@@ -1,14 +1,14 @@
 package com.serverchat;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EchoServer {
+public class EchoServer  {
+
 
     public static void main(String[] args) {
+
         // Try with Resources
         try (ServerSocket serverSocket = new ServerSocket(8181))  {
             System.out.println("Waiting for client to connect");
@@ -17,12 +17,29 @@ public class EchoServer {
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
+
+            // to send data to the client
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+            // to read data coming from the client
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // to read data from the keyboard
+            BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+
             while (true){
+                String str, str1;
+                while ((str = bufferedReader.readLine()) != null) {
+                    System.out.println(str);
+                    str1 = kb.readLine();
+
+                    // send to client
+                    printStream.println(str1);
+                }
                 String s = inputStream.readUTF();
                 if ("/end".equals(s)) {
                     break;
                 }
                 outputStream.writeUTF(" Echo "  + s);
+
             }
 
         } catch (IOException e) {
